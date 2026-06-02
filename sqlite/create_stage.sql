8915cbe97774d171dfceeed8a9d22368fd2ab943
filipesiqueira2022 <95728904+filipesiqueira2022@users.noreply.github.com>
2026-06-01 22:58:57 -0300

    Create stg_investimentos table and insert records
    Create a table for investment stages and insert initial records.

--- sqlite/create_stage.sql
+++ sqlite/create_stage.sql
@@ -1,1 +1,27 @@
+# Criação da tabela stage investimentos #
 
+CREATE TABLE stg_investimentos (
+    id_cliente INTEGER,
+    cliente TEXT,
+    produto TEXT,
+    valor REAL,
+    data_mov TEXT
+)
+
+#INSERINDO REGISTROS NA stage #
+
+INSERT INTO stg_investimentos 
+(id_clientE,cliente,produto,valor,data_mov)
+VALUES
+(1,'Filipe','CDB',1000,'01/06/2026')
+
+INSERT INTO stg_investimentos 
+(id_clientE,cliente,produto,valor,data_mov)
+VALUES
+(2,'Ana','FII',2500,'01/06/2026')
+
+
+INSERT INTO stg_investimentos 
+(id_clientE,cliente,produto,valor,data_mov)
+VALUES
+(3,'Pedro','Tesouro',5000,'01/06/2026')
